4734b11a2be946fe622e4734c418d54f54832473
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-01-31 19:42:04 +0300

    Add tests for isBalanced method

---
+++ tests/is_balanced.cpp
@@ -0,0 +1,24 @@
+#include "btree_fixture.h"
+
+TEST_F(BTreeTest, IsBalancedEmpty) {
+	EXPECT_EQ(empty.isBalanced(), true);
+}
+
+TEST_F(BTreeTest, IsBalancedNormalCases) {
+	EXPECT_EQ(oneItem.isBalanced(), true);
+	EXPECT_EQ(threeItems.isBalanced(), true);
+	EXPECT_EQ(balancedTree.isBalanced(), true);
+	EXPECT_EQ(tree1.isBalanced(), false);
+
+	balancedTree.insert(-3);
+	EXPECT_EQ(balancedTree.isBalanced(), false);
+
+	balancedTree.remove(-3);
+	EXPECT_EQ(balancedTree.isBalanced(), true);
+	balancedTree.insert(2);
+	balancedTree.insert(2);
+	EXPECT_EQ(balancedTree.isBalanced(), false);
+
+	balancedTree.clear();
+	EXPECT_EQ(balancedTree.isBalanced(), true);
+}
