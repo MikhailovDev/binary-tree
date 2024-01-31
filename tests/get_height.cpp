0ed58afcfe044f9fbf0a8cfa758e1da1d68c03a6
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-01-31 19:42:04 +0300

    Add tests for getHeight method

---
+++ tests/get_height.cpp
@@ -0,0 +1,24 @@
+#include "btree_fixture.h"
+
+TEST_F(BTreeTest, GetHeightEmpty) {
+	EXPECT_EQ(empty.getHeight(0), -1);
+}
+
+TEST_F(BTreeTest, GetHeightOneItem) {
+	EXPECT_EQ(oneItem.getHeight(0), -1);
+	EXPECT_EQ(oneItem.getHeight(1), 0);
+}
+
+TEST_F(BTreeTest, GetHeightNormalTree) {
+	EXPECT_EQ(balancedTree.getHeight(1), 2);
+	EXPECT_EQ(balancedTree.getHeight(0), 1);
+	EXPECT_EQ(balancedTree.getHeight(-2), 0);
+	EXPECT_EQ(balancedTree.getHeight(5), 0);
+	EXPECT_EQ(balancedTree.getHeight(*balancedTree.getMax()), 0);
+	EXPECT_EQ(balancedTree.getHeight(-1), -1);
+
+	balancedTree.insert(5);
+	balancedTree.insert(5);
+	balancedTree.insert(5);
+	EXPECT_EQ(balancedTree.getHeight(1), 5);
+}
