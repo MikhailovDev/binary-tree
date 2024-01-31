787213f88416c59ae971534c41de76bbd5d8c74e
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-01-31 19:50:39 +0300

    Add tests for clear method

---
+++ tests/clear.cpp
@@ -0,0 +1,25 @@
+#include "btree_fixture.h"
+
+TEST_F(BTreeTest, ClearEmpty) {
+	EXPECT_EQ(empty.getSize(), 0);
+	empty.clear();
+	EXPECT_EQ(empty.getSize(), 0);
+}
+
+TEST_F(BTreeTest, ClearOneItem) {
+	EXPECT_EQ(oneItem.getSize(), 1);
+	oneItem.clear();
+	EXPECT_EQ(oneItem.getSize(), 0);
+	EXPECT_EQ(oneItem.lookUp(1), nullptr);
+}
+
+TEST_F(BTreeTest, ClearNormalTree) {
+	EXPECT_EQ(balancedTree.getSize(), 6);
+	balancedTree.clear();
+	EXPECT_EQ(balancedTree.getSize(), 0);
+	EXPECT_EQ(balancedTree.lookUp(1), nullptr);
+	EXPECT_EQ(balancedTree.lookUp(0), nullptr);
+	EXPECT_EQ(balancedTree.lookUp(2), nullptr);
+	EXPECT_EQ(balancedTree.lookUp(-2), nullptr);
+	EXPECT_EQ(balancedTree.lookUp(5), nullptr);
+}
