f1d95bc5d1b2d265b8874ef4a8fc5fc9b08dd4f3
Gleb Mikhailov <555lifeisbeautiful5@gmail.com>
2024-02-05 08:29:46 +0300

    Add changes in CMakeLists

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justelife/VSCode_projects/Binary_tree
-# Build directory: /home/justelife/VSCode_projects/Binary_tree/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/justelife/VSCode_projects/Binary_tree/build/tests[1]_include.cmake")
-subdirs("googletest")
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justelife/VSCode_projects/Binary_tree/googletest
-# Build directory: /home/justelife/VSCode_projects/Binary_tree/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
